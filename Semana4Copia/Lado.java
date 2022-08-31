2f1fd9fecd5b67a4d0c7a414e536458efbcfd890
jjsanchezc <jjsanchezc@eafit.edu.co>
2022-08-31 07:04:59 -0500

    Añadí la documentación de los metodos

--- Semana4Copia/Lado.java
+++ Semana4Copia/Lado.java
@@ -4,6 +4,11 @@ public class Lado {
     public int a;
     public int b;
 
+    /**
+     * Metodo Constructor para la clase Lado, toma los puntos a tomarse en cuanta para poder hacer el lado
+     * @param a primer punto a tomar en cuenta
+     * @param b segundo punto a tomar en cuenta
+     */
     public Lado(int a, int b){
         this.a = a;
         this.b = b;
--- Semana4Copia/Punto.java
+++ Semana4Copia/Punto.java
@@ -1,27 +1,50 @@
 package Semana4Copia;
 
+
 public class Punto {
     public static double width = 600;
     public static double height = 400;
     public int x;
     public int y;
 
+    /**
+     * Constructor de la clase Punto
+     * @param x valor de x en coordenadas cartesianas
+     * @param y valor de y en coordenadas cartesianas
+     */
     public Punto(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * metodo getter del eje x
+     * @return retorna el valor de x en coordenadas cartesianas
+     */
     public double getXCart(){
         return this.x - width/2;
     }
 
+    /**
+     * metodo getter del eje y
+     * @return retorna el valor de y en coordenadas cartesianas
+     */
     public double getYCart(){
         return height/2 - this.y;
     }
 
+    /**
+     * Setter del eje x, pasa de coordenadas cartesaianas a las coordenadas de JFrame
+     * @param x valor del eje x en coordenadas cartesianas
+     */
     public void setXFromCart(double x){
         this.x = (int) (x + width/2);
     }
+
+    /**
+     * Setter del eje y, pasa de coordenadas cartseaianas a coordenadas JFrame
+     * @param y valor del eje y en coordenadas cartesianas
+     */
     public void setYFromCart(double y){
         this.y = (int) (height/2 - y);
     }
